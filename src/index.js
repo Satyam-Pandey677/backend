433e825c45e5c35c5f6695ed71dc1ca50841afd5
sanskar pandey <sanskarpandey9404@gmail.com>
2025-04-19 17:56:06 +0530

    databse connection code

---
+++ src/index.js
@@ -0,0 +1,30 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path :'./env'
+})
+
+connectDB()
+
+// import express from "express"
+// const app = express()
+
+// (async () => {
+//     try {
+//         mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//         app.on("error", () => {
+//             console.log("Error", error)
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listening on ${process.env.PORT} `)
+//         })
+//     } catch (error) {
+//         console.log(error.message)
+//     }
+// })()
+
